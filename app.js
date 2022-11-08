6e9b2aac371158eda2596ec4e8f1b4cdd6232ab0
Richard Djarbeng <richarddjarbeng@gmail.com>
2022-11-08 12:21:34 +0000

    add socket.io change port to 3000

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
-
+var socket = require("socket.io");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-
-var listener = app.listen(8080, function() {
+const PORT = 3000 || process.env.PORT;
+var listener = app.listen(PORT, function () {
   console.log("Listening on port " + listener.address().port);
 });
